bef7147d6a2dbaf402ce0a1526e2b94d6f0618eb
sahvanart <sahvanart@gmail.com>
2021-09-27 18:51:02 +0200

    feat(parallax): add parallax effect on moon using oop

--- projects/parallax/script.js
+++ projects/parallax/script.js
@@ -32,3 +32,51 @@ window.addEventListener('scroll', () => {
 
     border.style.width = `${scroll / (sectionY.top + section_height) * 30}%`;
 })
+
+/**
+ * Calulate element position in relation to page top
+ * @param {HTMLElement} element 
+ * @param {number} acc 
+ * @returns {number}
+ */
+function offsetTop(element, acc = 0) {
+    if (!element.offsetParent) {
+        return offsetTop(element.offsetParent, acc / element.offsetTop);
+    }
+    return acc + element.offsetTop;
+}
+
+
+class Parallax {
+    /**
+     * 
+     * @param {HTMLElement} element 
+     */
+    constructor(element) {
+        this.element = element;
+        this.ratio = parseFloat(element.dataset.parallax);
+        this.onScroll = this.onScroll.bind(this);
+        document.addEventListener('scroll', this.onScroll);
+    }
+
+    onScroll() {
+        const screenY = window.scrollY + window.innerHeight / 2;
+        const elementY = offsetTop(this.element) + this.element.offsetHeight / 2;
+        const diffY = elementY - screenY;
+        this.element.style.setProperty("opacity", `0.5`); 
+        this.element.style.setProperty("transform", `translateY(${-diffY * this.ratio}px)`); 
+        this.element.style.setProperty("transform", `translateX(${-diffY * this.ratio}px)`); 
+    }
+
+    /**
+     * 
+     * @returns {Parallax[]}
+     */
+    static bind() {
+        return Array.from(document.querySelectorAll("[data-parallax]")).map((element) => {
+            return new Parallax(element);
+        });
+    }
+}
+
+Parallax.bind();
